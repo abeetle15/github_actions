3d52b993fa9fb3aa6a8b49904a70e779a91f4f98
A Beetle <abeetle15@gmail.com>
2025-01-17 14:19:59 -0500

    include failed test

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,4 +5,5 @@ test("GET / should respond with 'Hello, World!'", async () => {
   const response = await request(server).get("/");
   expect(response.status).toBe(200);
   expect(response.text).toBe("Hello, World!");
+  expect(response.text).toBe("failed test");
 });
